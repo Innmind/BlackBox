7fef616f69826babd3097d4e9f5072858de69265
Baptiste Langlade <baptouuuu@gmail.com>
2019-04-21 16:18:03 +0200

    clean slate

--- fixture.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-declare(strict_types = 1);
-
-require 'vendor/autoload.php';
-
-use function Innmind\BlackBox\run;
-
-run('fixtures');
--- src/Exception/LogicException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-namespace Innmind\BlackBox\Exception;
-
-class LogicException extends \LogicException implements Exception
-{
-}
