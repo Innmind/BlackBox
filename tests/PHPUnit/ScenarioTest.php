026850fc57c3c94874346be5b28e240e4233eee3
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-29 15:54:49 +0200

    add phpunit integration

---
+++ tests/PHPUnit/ScenarioTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\BlackBox\PHPUnit;
+
+use Innmind\BlackBox\{
+    PHPUnit\Scenario,
+    Set\Integers,
+};
+use PHPUnit\Framework\TestCase;
+
+class ScenarioTest extends TestCase
+{
+    public function testCallingWithOnlyOneSet()
+    {
+        $scenario = new Scenario(Integers::of('foo'));
+
+        $called = 0;
+        $scenario->then(static function(int $foo) use (&$called): void {
+            ++$called;
+        });
+
+        $this->assertSame(100, $called);
+    }
+
+    public function testCallingWithMultipleSets()
+    {
+        $scenario = new Scenario(Integers::of('a'), Integers::of('b'));
+
+        $called = 0;
+        $scenario->then(static function(int $a, $b) use (&$called): void {
+            ++$called;
+        });
+
+        $this->assertSame(10000, $called);
+    }
+}
