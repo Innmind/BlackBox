24fbd53502cf3cd4d826ce41e40c3066d957c47e
Baptiste Langlade <baptouuuu@gmail.com>
2019-10-05 12:49:27 +0200

    add uuid set

---
+++ tests/Set/UuidTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\BlackBox\Set;
+
+use Innmind\BlackBox\{
+    Set\Uuid,
+    Set,
+};
+use PHPUnit\Framework\TestCase;
+
+class UuidTest extends TestCase
+{
+    public function testOf()
+    {
+        $uuids = Uuid::of();
+
+        $this->assertInstanceOf(Set::class, $uuids);
+        $this->assertCount(100, \iterator_to_array($uuids->values()));
+
+        foreach ($uuids->values() as $uuid) {
+            $this->assertRegExp(
+                '~^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$~',
+                $uuid
+            );
+        }
+    }
+}
