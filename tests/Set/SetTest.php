ed8583985e8bd5ef688eb014e0d85c840dae89b3
Baptiste Langlade <baptouuuu@gmail.com>
2019-10-13 10:46:21 +0200

    add set set

---
+++ tests/Set/SetTest.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\BlackBox\Set;
+
+use Innmind\BlackBox\Set;
+use Innmind\Immutable\Set as Structure;
+use PHPUnit\Framework\TestCase;
+
+class SetTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Set::class,
+            new Set\Set('string', new Set\Chars)
+        );
+    }
+
+    public function testOf()
+    {
+        $this->assertInstanceOf(
+            Set\Set::class,
+            Set\Set::of('string', new Set\Chars)
+        );
+    }
+
+    public function testGenerates100ValuesByDefault()
+    {
+        $sets = new Set\Set('string', new Set\Chars);
+
+        $this->assertInstanceOf(\Generator::class, $sets->values());
+        $this->assertCount(100, \iterator_to_array($sets->values()));
+
+        foreach ($sets->values() as $set) {
+            $this->assertInstanceOf(Structure::class, $set);
+            $this->assertSame('string', (string) $set->type());
+        }
+    }
+
+    public function testGeneratesSequencesOfDifferentSizes()
+    {
+        $sets = new Set\Set(
+            'string',
+            new Set\Chars,
+            Set\Integers::of(0, 50)
+        );
+        $sizes = [];
+
+        foreach ($sets->values() as $set) {
+            $sizes[] = $set->size();
+        }
+
+        $this->assertTrue(\count(\array_unique($sizes)) > 1);
+    }
+
+    public function testTake()
+    {
+        $sets1 = new Set\Set('string', new Set\Chars);
+        $sets2 = $sets1->take(50);
+
+        $this->assertNotSame($sets1, $sets2);
+        $this->assertInstanceOf(Set\Set::class, $sets2);
+        $this->assertCount(100, \iterator_to_array($sets1->values()));
+        $this->assertCount(50, \iterator_to_array($sets2->values()));
+    }
+
+    public function testFilter()
+    {
+        $sets1 = new Set\Set('string', new Set\Chars);
+        $sets2 = $sets1->filter(static function($set): bool {
+            return $set->size() % 2 === 0;
+        });
+
+        $this->assertNotSame($sets1, $sets2);
+        $this->assertInstanceOf(Set\Set::class, $sets2);
+
+        $values1 = \iterator_to_array($sets1->values());
+        $values2 = \iterator_to_array($sets2->values());
+        $values1 = \array_map(function($set) {
+            return $set->size() % 2;
+        }, $values1);
+        $values2 = \array_map(function($set) {
+            return $set->size() % 2;
+        }, $values2);
+        $values1 = \array_unique($values1);
+        $values2 = \array_unique($values2);
+        \sort($values1);
+
+        $this->assertSame([0, 1], \array_values($values1));
+        $this->assertSame([0], \array_values($values2));
+    }
+}
