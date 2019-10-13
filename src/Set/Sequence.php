2dfbcafb766d78712e0a1e6c4365c6f6771e172a
Baptiste Langlade <baptouuuu@gmail.com>
2019-10-13 10:32:19 +0200

    add sequence set

---
+++ src/Set/Sequence.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\BlackBox\Set;
+
+use Innmind\BlackBox\Set;
+use Innmind\Immutable\Sequence as Structure;
+
+/**
+ * {@inheritdoc}
+ */
+final class Sequence implements Set
+{
+    private $set;
+    private $sizes;
+    private $size;
+    private $predicate;
+
+    public function __construct(Set $set, Integers $sizes = null)
+    {
+        $this->set = $set;
+        $this->size = 100;
+        $this->sizes = ($sizes ?? Integers::of(0, 100))->take($this->size);
+        $this->predicate = static function(): bool {
+            return true;
+        };
+    }
+
+    public static function of(Set $set, Integers $sizes = null): self
+    {
+        return new self($set, $sizes);
+    }
+
+    public function take(int $size): Set
+    {
+        $self = clone $this;
+        $self->size = $size;
+        $self->sizes = $this->sizes->take($size);
+
+        return $self;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(callable $predicate): Set
+    {
+        $self = clone $this;
+        $self->predicate = function($value) use ($predicate): bool {
+            if (!($this->predicate)($value)) {
+                return false;
+            }
+
+            return $predicate($value);
+        };
+
+        return $self;
+    }
+
+    /**
+     * @return \Generator<Structure>
+     */
+    public function values(): \Generator
+    {
+        foreach ($this->sizes->values() as $size) {
+            $sequence = new Structure;
+            $values = $this->set->values();
+
+            while ($sequence->size() < $size && $values->valid()) {
+                $sequence = $sequence->add($values->current());
+                $values->next();
+            }
+
+            if (!($this->predicate)($sequence)) {
+                continue;
+            }
+
+            yield $sequence;
+        }
+    }
+}
