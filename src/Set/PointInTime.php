de5e46d172e9a9d69d277b91da5d7577c0646ebd
Baptiste Langlade <baptouuuu@gmail.com>
2019-11-30 11:28:57 +0100

    allow to specify a upper or lower bound for a point in time

--- src/Set/PointInTime.php
+++ src/Set/PointInTime.php
@@ -13,6 +13,49 @@ final class PointInTime
      */
     public static function any(): Set
     {
+        return self::yearRange(0, 9999);
+    }
+
+    /**
+     * @param string $point String representation of a date
+     * @return Set<Model>
+     */
+    public static function after(string $point): Set
+    {
+        $lower = new Model($point);
+
+        return self::yearRange($lower->year()->toInt(), 9999)->filter(static function($point) use ($lower): bool {
+            return $point->aheadOf($lower);
+        });
+    }
+
+    /**
+     * @param string $point String representation of a date
+     * @return Set<Model>
+     */
+    public static function before(string $point): Set
+    {
+        $upper = new Model($point);
+
+        return self::yearRange(0, $upper->year()->toInt())->filter(static function($point) use ($upper): bool {
+            return $upper->aheadOf($point);
+        });
+    }
+
+    /**
+     * @deprecated
+     * @see self::any()
+     */
+    public static function of(): Set
+    {
+        return self::any();
+    }
+
+    /**
+     * @return Set<Model>
+     */
+    private static function yearRange(int $lowerBound, int $upperBound): Set
+    {
         return Composite::of(
             static function(
                 int $year,
@@ -27,7 +70,7 @@ final class PointInTime
             ): Model {
                 return new Model("$year-$month-{$day}T$hour:$minute:$second$offsetDirection$hourOffset:$minuteOffset");
             },
-            Integers::between(0, 9999),
+            Integers::between($lowerBound, $upperBound),
             Integers::between(1, 12),
             Integers::between(1, 31),
             Integers::between(0, 23),
@@ -37,14 +80,5 @@ final class PointInTime
             Integers::between(0, 12),
             Elements::of('00', '15', '30', '45')
         )->take(100);
-    }
-
-    /**
-     * @deprecated
-     * @see self::any()
-     */
-    public static function of(): Set
-    {
-        return self::any();
     }
 }
