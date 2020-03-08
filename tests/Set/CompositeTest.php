7fa7f52950456e6212a55d6df93c665dcdc9bf59
Baptiste Langlade <baptouuuu@gmail.com>
2020-03-08 12:23:09 +0100

    add test to make sure shrunk composite shrinks all its underlyng values

--- tests/Set/CompositeTest.php
+++ tests/Set/CompositeTest.php
@@ -318,4 +318,32 @@ class CompositeTest extends TestCase
             $this->assertNotSame($a->prop, $b->prop);
         }
     }
+
+    public function testShrinkAllValuesToTheirMinimumPossible()
+    {
+        $set = Composite::immutable(
+            function(string ...$args) {
+                return implode('', $args);
+            },
+            Set\Strings::between(0, 10),
+            Set\Strings::between(0, 10),
+            Set\Strings::between(0, 10),
+        );
+
+        foreach ($set->values() as $value) {
+            $a = $value;
+            while ($a->shrinkable()) {
+                $a = $a->shrink()->a();
+            }
+
+            $this->assertSame('', $a->unwrap());
+
+            $b = $value;
+            while ($b->shrinkable()) {
+                $b = $b->shrink()->b();
+            }
+
+            $this->assertSame('', $b->unwrap());
+        }
+    }
 }
