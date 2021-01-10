4f56bcdd951a971c0baf76fd0c704bb04f4f0ce2
Baptiste Langlade <baptouuuu@gmail.com>
2021-01-10 16:16:27 +0100

    skip tests when the reverse regex is not installed

--- tests/Set/EmailTest.php
+++ tests/Set/EmailTest.php
@@ -10,6 +10,7 @@ use Innmind\BlackBox\{
     PHPUnit\BlackBox,
     Random\MtRand,
 };
+use ReverseRegex\Lexer;
 
 class EmailTest extends TestCase
 {
@@ -17,6 +18,8 @@ class EmailTest extends TestCase
 
     public function testAny()
     {
+        $this->skipIfUninstalled();
+
         $emails = Email::any();
 
         $this->assertInstanceOf(Set::class, $emails);
@@ -31,6 +34,8 @@ class EmailTest extends TestCase
 
     public function testEmailsAreValid()
     {
+        $this->skipIfUninstalled();
+
         $this
             ->forAll(Email::any())
             ->then(function($email) {
@@ -40,10 +45,19 @@ class EmailTest extends TestCase
 
     public function testEmailsAreShrinkable()
     {
+        $this->skipIfUninstalled();
+
         $emails = Email::any();
 
         foreach ($emails->values(new MtRand) as $email) {
             $this->assertTrue($email->shrinkable());
+        }
+    }
+
+    private function skipIfUninstalled()
+    {
+        if (!\class_exists(Lexer::class)) {
+            $this->markTestSkipped();
         }
     }
 }
