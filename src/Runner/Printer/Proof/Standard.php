2c9e18e777461cfb5b03d91837d731913d9b6587
Baptiste Langlade <baptouuuu@gmail.com>
2023-05-20 15:01:24 +0200

    display the stack trace to the failure

--- src/Runner/Printer/Proof/Standard.php
+++ src/Runner/Printer/Proof/Standard.php
@@ -60,7 +60,49 @@ final class Standard implements Proof
                 true,
             ) ?? '',
         );
-        // TODO print the detail
+
+        /**
+         * @var list<array{
+         *      file?: string,
+         *      line?: int,
+         * }> $frame
+         */
+        $trace = $failure->assertion()->getTrace();
+        $output("\n");
+
+        foreach ($trace as $frame) {
+            if (!\array_key_exists('file', $frame)) {
+                continue;
+            }
+
+            if (!\array_key_exists('line', $frame)) {
+                continue;
+            }
+
+            // do not render the internal calls to the runner, this path is the
+            // one either locally on my machine or as a project dependency via
+            // composer
+            if (
+                \str_contains($frame['file'], 'innmind/black-box/src/Runner') ||
+                \str_contains($frame['file'], 'innmind/black-box/src/Application.php')
+            ) {
+                continue;
+            }
+
+            // same goal as above but for the GitHub Actions
+            if (
+                \str_contains($frame['file'], '/home/runner/work/BlackBox/BlackBox/src/Runner') ||
+                \str_contains($frame['file'], '/home/runner/work/BlackBox/BlackBox/src/Application.php')
+            ) {
+                continue;
+            }
+
+            $output(\sprintf(
+                "%s:%s\n",
+                $frame['file'],
+                $frame['line'],
+            ));
+        }
     }
 
     public function end(IO $output, IO $error): void
