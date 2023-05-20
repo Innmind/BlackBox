9aa537a9d0456cc1e5289ea5ba996ec71c514cc4
Baptiste Langlade <baptouuuu@gmail.com>
2023-05-20 11:00:43 +0200

    implement shrinking

--- src/Runner/Runner/WithShrinking.php
+++ src/Runner/Runner/WithShrinking.php
@@ -26,6 +26,69 @@ final class WithShrinking
         Assert $assert,
         Value $scenario,
     ): void {
-        // TODO
+        try {
+            $scenario->unwrap()($assert);
+            $print->success($output, $error);
+        } catch (Failure $e) {
+            if (!$scenario->shrinkable()) {
+                throw $e;
+            }
+
+            $this->shrink(
+                $e,
+                $print,
+                $output,
+                $error,
+                $assert,
+                $scenario,
+            );
+        }
+    }
+
+    /**
+     * @param Value<Scenario> $scenario
+     *
+     * @throws Failure
+     */
+    private function shrink(
+        Failure $previousFailure,
+        Printer\Proof $print,
+        IO $output,
+        IO $error,
+        Assert $assert,
+        Value $scenario,
+    ): void {
+        $previousStrategy = $scenario;
+        $dichotomy = $scenario->shrink();
+
+        do {
+            $currentStrategy = $dichotomy->a();
+
+            try {
+                $currentStrategy->unwrap()($assert);
+                $currentStrategy = $dichotomy->b();
+                $currentStrategy->unwrap()($assert);
+            } catch (Failure $e) {
+                if ($currentStrategy->shrinkable()) {
+                    $dichotomy = $currentStrategy->shrink();
+                    $previousFailure = $e;
+                    $previousStrategy = $currentStrategy;
+
+                    $print->shrunk($output, $error);
+
+                    continue;
+                }
+
+                // current strategy no longer shrinkable so it means we reached
+                // a leaf of our search tree meaning the current exception is the
+                // last one we can obtain
+                throw $e;
+            }
+
+            // when a and b work then the previous failure has been generated
+            // with the smallest values possible
+            throw $previousFailure;
+            // we can use an infinite condition here since all exits are covered
+        } while (true);
     }
 }
