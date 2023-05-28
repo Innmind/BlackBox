f82c1df80acec1d5f440f568214ff8bbdee35d3b
Baptiste Langlade <baptouuuu@gmail.com>
2023-05-28 11:16:11 +0200

    increase code coverage

--- blackbox.php
+++ blackbox.php
@@ -11,7 +11,11 @@ use Innmind\BlackBox\{
 
 Application::new($argv)
     ->codeCoverage(
-        CodeCoverage::of(__DIR__.'/src/')
+        CodeCoverage::of(
+            __DIR__.'/src/',
+            __DIR__.'/proofs/',
+            __DIR__.'/fixtures/',
+        )
             ->dumpTo('coverage.clover')
             ->enableWhen(\getenv('ENABLE_COVERAGE') !== false),
     )
