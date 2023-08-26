8989fb119e5118d67695b5e65ca74ac7ea30c288
Baptiste Langlade <baptouuuu@gmail.com>
2023-08-26 09:28:23 +0200

    fix removing the wrong test

--- phpunit/PHPUnit/BlackBoxTest.php
+++ phpunit/PHPUnit/BlackBoxTest.php
@@ -16,6 +16,28 @@ class BlackBoxTest extends TestCase
 {
     use BlackBox;
 
+    public function testTrait()
+    {
+        $class = new class() {
+            use BlackBox;
+
+            public function assert(): int
+            {
+                $called = 0;
+                $this
+                    ->forAll(Set\Integers::any())
+                    ->then(static function() use (&$called) {
+                        ++$called;
+                    });
+
+                return $called;
+            }
+        };
+
+        // 200 because it reads the `BLACKBOX_SET_SIZE` env var
+        $this->assertSame(200, $class->assert());
+    }
+
     public function testDoesntFailWhenTheExceptionIsExpected()
     {
         $this
