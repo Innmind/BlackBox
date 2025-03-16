f7535691a1878d469798fcd6ebd414aa17c05858
Baptiste Langlade <baptiste.langlade@hey.com>
2025-03-16 14:39:36 +0100

    rename unwrap to map

--- src/Set/Value/Mutable.php
+++ src/Set/Value/Mutable.php
@@ -19,11 +19,11 @@ final class Mutable
     /**
      * @psalm-mutation-free
      *
-     * @param \Closure(mixed): (T|Seed<T>) $unwrap
+     * @param \Closure(mixed): (T|Seed<T>) $map
      */
     private function __construct(
         private mixed $source,
-        private \Closure $unwrap,
+        private \Closure $map,
     ) {
     }
 
@@ -33,13 +33,13 @@ final class Mutable
      * @template V
      *
      * @param V|Seed<V> $value
-     * @param \Closure(mixed): (T|Seed<T>) $unwrap
+     * @param \Closure(mixed): (T|Seed<T>) $map
      *
      * @return self<V>
      */
-    public static function of($value, \Closure $unwrap): self
+    public static function of($value, \Closure $map): self
     {
-        return new self($value, $unwrap);
+        return new self($value, $map);
     }
 
     /**
@@ -63,8 +63,8 @@ final class Mutable
      */
     public function map(callable $map): self
     {
-        $previous = $this->unwrap;
-        $unwrap = static function(mixed $source) use ($map, $previous): mixed {
+        $previous = $this->map;
+        $map = static function(mixed $source) use ($map, $previous): mixed {
             $value = $previous($source);
 
             if ($value instanceof Seed) {
@@ -85,7 +85,7 @@ final class Mutable
 
         return new self(
             $this->source,
-            $unwrap,
+            $map,
         );
     }
 
@@ -99,7 +99,7 @@ final class Mutable
      */
     public function unwrap()
     {
-        $value = ($this->unwrap)($this->source);
+        $value = ($this->map)($this->source);
 
         // This is not ideal to hide the seeded value this way and to hijack
         // the shrinking system in self::shrinkable() and self::shrink() as it
