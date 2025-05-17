51793555ac0db049e20c45a04811f4b752db5431
Baptiste Langlade <baptiste.langlade@hey.com>
2025-05-17 14:44:52 +0200

    explain why the predicate is still sent through the set stack

--- src/Set/Implementation.php
+++ src/Set/Implementation.php
@@ -20,6 +20,10 @@ interface Implementation
      */
     public function __invoke(
         Random $random,
+        // The predicate is still sent through the composition stack as it
+        // allows implementations like `Elements` to rule out values from ever
+        // being generated as these would never match the predicate when verified
+        // above in the stack.
         \Closure $predicate,
     ): \Generator;
 }
